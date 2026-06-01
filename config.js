6d75b6cc3fbf7883dd07ae3111b8b0af16068083
tiyanzxbellas <tiyabbella@gmail.com>
2026-06-01 18:36:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteName: "csite",
   authorName: "tyns",
-  profilePicture: "https://c.tiyan.biz.id/pazA.webp",
-  profileBanner: "https://c.tiyan.biz.id/pazA.webp",
+  profilePicture: "https://c.tyn.biz.id//8mJM.webp",
+  profileBanner: "https://c.tyn.biz.id//8mJM.webp",
   aboutText: "A personal project for file hosting and URL shortening, built with simplicity and efficiency in mind.",
-  backgroundImage: "https://c.tiyan.biz.id/pazA.webp"
+  backgroundImage: "https://c.tyn.biz.id//8mJM.webp"
 };
