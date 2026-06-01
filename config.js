fd956693983f430302a19a3731ac6d2b14421926
tiyanzxbellas <tiyabbella@gmail.com>
2026-06-01 16:06:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  siteName: "NefuSite",
-  authorName: "NefuSoft",
-  profilePicture: "https://cdn.nefyu.my.id/csy1.jpg",
-  profileBanner: "https://cdn.nefyu.my.id/cgxl.png",
+  siteName: "csite",
+  authorName: "tyns",
+  profilePicture: "https://c.tiyan.biz.id/pazA.webp",
+  profileBanner: "https://c.tiyan.biz.id/pazA.webp",
   aboutText: "A personal project for file hosting and URL shortening, built with simplicity and efficiency in mind.",
-  backgroundImage: "https://cdn.nefyu.my.id/1lsq.webp"
+  backgroundImage: "https://c.tiyan.biz.id/pazA.webp"
 };
